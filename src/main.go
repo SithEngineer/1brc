4a04518c03b3ce2da38fecf043446d052a3301bf
Fábio <fabio.a.s.dias+github@gmail.com>
2024-03-27 19:32:50 +0000

    Pass the read buffer page from the file into a channel

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 )
 
 // Measuring time by:
@@ -14,85 +15,34 @@ import (
 // ❯ multitime -n 10 ./read -input measurements-1brc.txt
 // multitime is available at https://tratt.net/laurie/src/multitime/
 
+// reading a file
 /*
             Mean        Std.Dev.    Min         Median      Max
-real        6.888       0.869       5.850       6.662       8.776
-user        0.005       0.001       0.002       0.005       0.006
-sys         1.976       0.092       1.795       1.978       2.130
-*/
-// const bufferSize int = 256 * 1024 * 1024
-
-/*
-            Mean        Std.Dev.    Min         Median      Max
-real        6.195       0.743       5.813       5.833       8.362
-user        0.009       0.001       0.007       0.009       0.010
-sys         1.889       0.074       1.769       1.893       2.053
-*/
-// const bufferSize int = 64 * 1024 * 1024
-
-/*
-            Mean        Std.Dev.    Min         Median      Max
-real        5.864       0.086       5.796       5.827       6.106
-user        0.014       0.001       0.012       0.014       0.015
-sys         1.884       0.044       1.769       1.900       1.920
-*/
-//const bufferSize int = 32 * 1024 * 1024
-
-/*
-	Mean        Std.Dev.    Min         Median      Max
-
 real        5.857       0.121       5.792       5.819       6.217
 user        0.024       0.002       0.021       0.024       0.026
 sys         1.876       0.039       1.789       1.888       1.917
 */
+
+// reading a file and copying the buffer into a channel
+/*
+            Mean        Std.Dev.    Min         Median      Max
+real        7.074       1.335       5.928       6.507       9.251
+user        1.156       0.005       1.149       1.156       1.164
+sys         1.658       0.076       1.556       1.634       1.772
+*/
+
 const bufferSize int = 16 * 1024 * 1024
 
-/*
-            Mean        Std.Dev.    Min         Median      Max
-real        5.834       0.114       5.773       5.795       6.172
-user        0.028       0.001       0.027       0.028       0.030
-sys         1.853       0.073       1.689       1.874       1.969
-*/
-// const bufferSize int = 8 * 1024 * 1024
-
-/*
-            Mean        Std.Dev.    Min         Median      Max
-real        6.084       0.117       6.028       6.044       6.432
-user        0.052       0.001       0.050       0.052       0.054
-sys         1.865       0.017       1.836       1.861       1.893
-*/
-// const bufferSize int = 1024 * 1024
-
-/*
-	          Mean        Std.Dev.    Min         Median      Max
-real        6.168       0.103       6.108       6.128       6.469
-user        0.087       0.001       0.085       0.087       0.090
-sys         2.008       0.019       1.969       2.011       2.030
-*/
-// const bufferSize int = 512 * 1024
-
-/*
-            Mean        Std.Dev.    Min         Median      Max
-real        10.086      0.203       9.923       9.956       10.563
-user        2.659       0.006       2.652       2.657       2.674
-sys         6.897       0.018       6.871       6.892       6.927
-*/
-// const bufferSize int = 1 * 1024
-
-func main() {
-	inputFile := flag.String("input", "", "measurements file to be processed")
-	flag.Parse()
-	if *inputFile == "" {
-		log.Fatal("measurements file not provided")
-	}
-
-	f, err := os.Open(*inputFile)
+func readFile(filename string, bufPageChan chan []byte) {
+	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
+	defer close(bufPageChan)
 
 	buf := make([]byte, bufferSize)
+
 	for {
 		bytesRead, err := f.Read(buf)
 		if bytesRead == 0 {
@@ -101,5 +51,29 @@ func main() {
 		if err != nil && !errors.Is(err, io.EOF) && !errors.Is(err, io.ErrUnexpectedEOF) {
 			panic(fmt.Errorf("reading to buffer: %w", err))
 		}
+		bufCopy := make([]byte, bytesRead)
+		copy(bufCopy, buf[:bytesRead])
+		bufPageChan <- bufCopy
 	}
+}
+
+func readBufferPages(bufPageChan chan []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _ = range bufPageChan {
+	}
+}
+
+func main() {
+	inputFile := flag.String("input", "", "measurements file to be processed")
+	flag.Parse()
+	if *inputFile == "" {
+		log.Fatal("measurements file not provided")
+	}
+
+	var wg sync.WaitGroup
+	bufPageChan := make(chan []byte, 100)
+	wg.Add(1)
+	go readBufferPages(bufPageChan, &wg)
+	go readFile(*inputFile, bufPageChan)
+	wg.Wait()
 }
